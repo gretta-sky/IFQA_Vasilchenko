9cab5ea634821bac2fca8a14298549bc6ab47686
Margarita <mm8595284@mail.ru>
2026-01-18 17:18:24 +0300

    Изменён язык и подкорректирована работа браузера

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -22,26 +23,13 @@ public class Hooks {
         Configuration.browser = "chrome";
         Configuration.baseUrl = getBaseUrl();
         Configuration.timeout = Duration.ofSeconds(15).toMillis();
-        Configuration.browserSize = "1920x1080";
         Configuration.headless = false;
         Configuration.holdBrowserOpen = false;
-
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
         Configuration.browserCapabilities = options;
     }
 
-    @Before
-    public void openBlankPage() {
-        Selenide.open("about:blank");
-    }
-
-    @Before
+    @BeforeStep
     public void maximizeBrowser() {
         maximizeToFullScreen();
     }
